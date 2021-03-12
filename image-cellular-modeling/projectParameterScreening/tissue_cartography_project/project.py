134a0323c5a89d84c94e2466e282b17a3928475f
nil23121994 <68352460+nil23121994@users.noreply.github.com>
2021-03-11 23:14:46 -0500

    Update project.py
    Added the flow operation for simulating the geometry file using surface evolver

--- image-cellular-modeling/projectParameterScreening/tissue_cartography_project/project.py
+++ image-cellular-modeling/projectParameterScreening/tissue_cartography_project/project.py
@@ -15,7 +15,7 @@ from geometry_writer import geometryWriter
 # Checking if an operation has been executed by checking f the initialization file exists
 @FlowProject.label
 def SE_file_exist_check(job):
-	return os.path.isfile("wingDisc.fe")
+	return os.path.isfile(job.fn("wingDisc.fe"))
 
 
 #a) write_SE_initialization_file has been defined as a function under the Flowproject class
@@ -25,7 +25,18 @@ def SE_file_exist_check(job):
 @FlowProject.operation
 @FlowProject.post(SE_file_exist_check)
 def write_SE_initialization_file(job):
-	geometryWriter(job.sp.parameter_model, job.sp.parameter_pressure, job.sp.output_file_name)
+	geometryWriter(job.sp.parameter_model, job.sp.parameter_pressure, job.fn(job.sp.output_file_name))
+
+
+# Defining a project operation for simulating the output geometry files
+@FlowProject.operation
+@flow.with_job
+@flow.cmd # It ensures that the functions returns a hell command through this decorator
+def simulate_SE_file(job):
+	# -x ensures that evolver stops after any error messages. 
+	# Genral synatx: evolver [-adehimqwxy] [-f file] [-pN] [datafile]
+	return "evolver -x wingdisc.fe"
+
 	
 if __name__ == '__main__':
 	FlowProject().main()	
